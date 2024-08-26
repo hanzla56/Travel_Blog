98f6a1b951a03d13d098855d93caa3cda05f3ef0
Ubuntu <ubuntu@ip-172-31-63-79.ec2.internal>
2024-08-26 10:47:25 +0000

    add files

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8@tc*4*+sk_q-2$7=i(-i$-oyyb#2%f7!#13ehn54dw-cg!^#y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
